1c7a7456e216953a047b51834a3f50839e9b59b3
SloanMariot <smariot@gmail.com>
2024-07-24 15:10:36 +0200

    init Docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("D:\\Documents\\3e\\git\\Docusaurus\\Docusaurus-ci-exam\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("D:\\Documents\\3e\\git\\Docusaurus\\Docusaurus-ci-exam\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("D:\\Documents\\3e\\git\\Docusaurus\\Docusaurus-ci-exam\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("D:\\Documents\\3e\\git\\Docusaurus\\Docusaurus-ci-exam\\src\\css\\custom.css"),
+];
